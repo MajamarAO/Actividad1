f4af5054b1f6456e11b0dac89da30c7d981e388a
MajamarAO <matiasalvarezoviedo@gmail.com>
2024-04-16 20:23:10 -0300

    init commit

--- main.cs
+++ main.cs
@@ -8,17 +8,20 @@ class Program
       //ciframos
       Console.WriteLine("Ingrese la frase a cifrar");
         string msj = Console.ReadLine();
-      
+
         while (string.IsNullOrEmpty(msj))
         {
             Console.WriteLine("Ingrese un mensaje no vacio");
             msj = Console.ReadLine();
         }
       Console.WriteLine("Ingrese la distancia");
-      string input = Console.ReadLine();
-        Program program = new Program();
-      
-        string msjcif = CifradoCesar7(msj);
+      int dist;
+      while (!int.TryParse(Console.ReadLine(), out dist) || dist <= 0)
+      {
+          Console.WriteLine("Por favor ingrese un entero válido:");
+      }
+
+        string msjcif = CifradoCesar(msj, dist);
         Console.WriteLine("Mensaje cifrado: " + msjcif);
 
       //desciframos
@@ -30,30 +33,36 @@ class Program
             Console.WriteLine("Ingrese un mensaje no vacio");
             msjd = Console.ReadLine();
         }
+      Console.WriteLine("Ingrese la distancia");
+      int distd;
+      while (!int.TryParse(Console.ReadLine(), out distd) || distd <= 0)
+      {
+          Console.WriteLine("Por favor ingrese un entero válido:");
+      }
 
-        string msjdcif = desCifradoCesar7(msjd);
+        string msjdcif = desCifradoCesar(msjd, distd);
         Console.WriteLine("Mensaje descifrado: " + msjdcif);
     }
 
-    static string CifradoCesar7(string mensaje)
+    static string CifradoCesar(string mensaje, int distancia)
     {
         string mensajeCifrado = "";
         foreach (char letra in mensaje)
         {
             int indice = alfabeto.IndexOf(letra);
-            char letraCifrada = alfabeto[(indice + 7) % alfabeto.Length];
+            char letraCifrada = alfabeto[(indice + distancia) % alfabeto.Length];
             mensajeCifrado += letraCifrada;
 
         }
         return mensajeCifrado;
     }
-  static string desCifradoCesar7(string mensaje)
+  static string desCifradoCesar(string mensaje, int distancia)
   {
       string mensajeCifrado = "";
       foreach (char letra in mensaje)
       {
           int indice = alfabeto.IndexOf(letra);
-          char letraCifrada = alfabeto[(indice - 7 + alfabeto.Length) % alfabeto.Length];
+          char letraCifrada = alfabeto[(indice - distancia + alfabeto.Length) % alfabeto.Length];
           mensajeCifrado += letraCifrada;
 
       }
